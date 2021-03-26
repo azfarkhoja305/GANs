9ffabf52a149d6d55471628f9f0f6d3e8372d369
Alexander Peplowski <alexander.peplowski@gmail.com>
2021-03-26 12:19:33 -0400

    WIP: debugging tensor shape mismatch

--- debug_DC_GAN_CIFAR.py
+++ debug_DC_GAN_CIFAR.py
@@ -17,26 +17,29 @@ from torchsummary import summary
 from datasets import ImageDataset
 from utils.utils import check_gpu, display_images
 from generator import Generator
-from discriminator import Discriminator
+from ViT_discriminator import Discriminator
+from types import SimpleNamespace
 
 device = check_gpu()
-print(f'Using device: {device}')
+print(f"Using device: {device}")
 
-dataset =  ImageDataset('cifar_10', batch_sz = 256)
+dataset = ImageDataset("cifar_10", batch_sz=256)
 
 # display_images(dataset.train_loader, max_idx=256)
 
 Gen = Generator().to(device)
-summary(Gen,(128,1))
+summary(Gen, (128, 1))
 
-Dis = Discriminator().to(device)
-summary(Dis,(3,32,32))
+# Dis = Discriminator().to(device)
+args = SimpleNamespace(**{"d_depth": 7, "df_dim": 384, "img_size": 48, "patch_size": 4})
+Dis = Discriminator(args).to(device)
+summary(Dis, (3, 32, 32))
 
 loss_fn = nn.BCEWithLogitsLoss()
 fixed_noise = torch.randn(64, 128, device=device)
-real_label = 1.
-fake_label = 0.
-lr,beta1 = 3e-4, 0
+real_label = 1.0
+fake_label = 0.0
+lr, beta1 = 3e-4, 0
 optG = optim.AdamW(Gen.parameters(), lr=lr, betas=(beta1, 0.999))
 optD = optim.AdamW(Dis.parameters(), lr=lr, betas=(beta1, 0.999))
 
@@ -64,14 +67,14 @@ for epoch in range(num_epochs):
         # Forward pass real batch through D
         output = Dis(real).view(-1)
         # Calculate loss on all-real batch
-        errD_real = loss_fn(output,label)
+        errD_real = loss_fn(output, label)
         # Calculate gradients for D in backward pass
         errD_real.backward()
         D_x = torch.sigmoid(output).mean().item()
 
         ## Train with all-fake batch
         # Generate batch of latent vectors
-        noise = torch.randn(b_size, 128, 1,1, device=device)
+        noise = torch.randn(b_size, 128, 1, 1, device=device)
         # Generate fake image batch with G
         fake = Gen(noise)
         label = torch.full_like(label, fake_label)
@@ -91,7 +94,9 @@ for epoch in range(num_epochs):
         # (2) Update G network: maximize log(D(G(z)))
         ###########################
         Gen.zero_grad()
-        label = torch.full_like(label,real_label) # fake labels are real for generator cost
+        label = torch.full_like(
+            label, real_label
+        )  # fake labels are real for generator cost
         # Since we just updated D, perform another forward pass of all-fake batch through D
         output = Dis(fake).view(-1)
         # Calculate G's loss based on this output
@@ -102,33 +107,35 @@ for epoch in range(num_epochs):
         # Update G
         optG.step()
 
-#         # Output training stats
-#         if (i+1) %100 == 0:
-#             print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
-# #                 % (epoch, num_epochs, i, len(dataset.train_loader),
-#                      errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
-        
+        #         # Output training stats
+        #         if (i+1) %100 == 0:
+        #             print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
+        # #                 % (epoch, num_epochs, i, len(dataset.train_loader),
+        #                      errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
+
         # Save Losses for plotting later
         G_losses.append(errG.item())
         D_losses.append(errD.item())
 
         # Check how the generator is doing by saving G's output on fixed_noise
-        if (iters % 500 == 0) or ((epoch == num_epochs-1) and (i == len(dataset.train_loader)-1)):
+        if (iters % 500 == 0) or (
+            (epoch == num_epochs - 1) and (i == len(dataset.train_loader) - 1)
+        ):
             with torch.no_grad():
                 fake = Gen(fixed_noise).detach().cpu()
             img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
 
-        iters +=1
+        iters += 1
 
-_, axs = plt.subplots(1,2,figsize=(15,15))
-display_images(dataset.train_loader,ax = axs[0], max_idx=256)
-display_images(img_list, ax = axs[1], max_idx=256)
+_, axs = plt.subplots(1, 2, figsize=(15, 15))
+display_images(dataset.train_loader, ax=axs[0], max_idx=256)
+display_images(img_list, ax=axs[1], max_idx=256)
 plt.tight_layout()
 
-plt.figure(figsize=(10,5))
+plt.figure(figsize=(10, 5))
 plt.title("Generator and Discriminator Loss During Training")
-plt.plot(G_losses,label="G")
-plt.plot(D_losses,label="D")
+plt.plot(G_losses, label="G")
+plt.plot(D_losses, label="D")
 plt.xlabel("iterations")
 plt.ylabel("Loss")
 plt.legend()
@@ -138,13 +145,13 @@ plt.show()
 
 from utils.torch_fid_score import get_fid
 
-stat_path = Path('fid_stats/cifar_10_valid_fid_stats.npz')
+stat_path = Path("fid_stats/cifar_10_valid_fid_stats.npz")
 score = get_fid(Gen, 128, 10000, 256, stat_path)
 print(f"\nFID score: {score}")
 
-rc('animation', html='jshtml')
-fig = plt.figure(figsize=(8,8))
+rc("animation", html="jshtml")
+fig = plt.figure(figsize=(8, 8))
 plt.axis("off")
-ims = [[plt.imshow(np.transpose(i,(1,2,0)), animated=True)] for i in img_list]
+ims = [[plt.imshow(np.transpose(i, (1, 2, 0)), animated=True)] for i in img_list]
 ani = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
 ani
